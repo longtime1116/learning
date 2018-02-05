5d578aa148efffba04b08da790b8ec9e83bb76d0
shumo.tokinaga <longtime1116@gmail.com>
2018-02-05 19:58:34 +0900

    p47 extract total_charge

--- refactoring_ruby/chapter_1/sample.rb
+++ refactoring_ruby/chapter_1/sample.rb
@@ -19,18 +19,18 @@ class Rental
   end
 
   def charge
-      result = 0
-      case movie.price_code
-      when Movie::REGULAR
-        result += 2
-        result += (days_rented - 2) * 1.5 if days_rented > 2
-      when Movie::NEW_RELEASE
-        result += days_rented * 3
-      when Movie::CHILDRENS
-        result += 1.5
-        result += (days_rented - 3) * 1.5 if days_rented > 3
-      end
-      result
+    result = 0
+    case movie.price_code
+    when Movie::REGULAR
+      result += 2
+      result += (days_rented - 2) * 1.5 if days_rented > 2
+    when Movie::NEW_RELEASE
+      result += days_rented * 3
+    when Movie::CHILDRENS
+      result += 1.5
+      result += (days_rented - 3) * 1.5 if days_rented > 3
+    end
+    result
   end
 
   def frequent_renter_points
@@ -57,18 +57,27 @@ class Customer
   end
 
   def statement
-    total_amount, frequent_renter_points = 0, 0
+    frequent_renter_points = 0
     result = "Rental Record for #{@name}\n"
     @rentals.each do |element|
       frequent_renter_points += element.frequent_renter_points
 
       # show figures for this rental
       result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
-      total_amount += element.charge
     end
     # add footer lines
-    result += "Amount owed is #{total_amount}\n"
+    result += "Amount owed is #{total_charge}\n"
     result += "You earned #{frequent_renter_points} frequent renter points"
+    result
+  end
+
+  private
+
+  def total_charge
+    result = 0
+    @rentals.each do |element|
+      result += element.charge
+    end
     result
   end
 end
