dc8a8261e337080e22c46d40f4965808ed7f84f0
shumo.tokinaga <longtime1116@gmail.com>
2018-02-05 19:25:33 +0900

    p32

--- refactoring_ruby/chapter_1/sample.rb
+++ refactoring_ruby/chapter_1/sample.rb
@@ -35,8 +35,27 @@ class Customer
     total_amount, frequent_renter_points = 0, 0
     result = "Rental Record for #{@name}\n"
     @rentals.each do |element|
+      this_amount = amount_for(element)
+
+      # add frequent renter points
+      frequent_renter_points += 1
+      # add bonus for a two day new release rental
+      if element.movie.price_code == Movie.NEW_RELEASE && element.days_rented > 1
+        frequent_renter_points += 1
+      end
+
+      # show figures for this rental
+      result += "\t" + element.movie.title + "\t" + this_amount.to_s + "\n"
+      total_amount += this_amount
+    end
+    # add footer lines
+    result += "Amount owed is #{total_amount}\n"
+    result += "You earned #{frequent_renter_points} frequent renter points"
+    result
+  end
+
+  def amount_for(element)
       this_amount = 0
-      # determine amounts for each line
       case element.movie.price_code
       when Movie::REGULAR
         this_amount += 2
@@ -47,19 +66,5 @@ class Customer
         this_amount += 1.5
         this_amount += (element.days_rented - 3) * 1.5 if element.days_rented > 3
       end
-      # add frequent renter points
-      frequent_renter_points += 1
-      # add bonus for a two day new release rental
-      if element.movie.price_code == Movie.NEW_RELEASE && element.days_rented > 1
-        frequent_renter_points += 1
-      end
-      # show figures for this rental
-      result += "\t" + element.movie.title + "\t" + this_amount.to_s + "\n"
-      total_amount += this_amount
-    end
-    # add footer lines
-    result += "Amount owed is #{total_amount}\n"
-    result += "You earned #{frequent_renter_points} frequent renter points"
-    result
   end
 end
