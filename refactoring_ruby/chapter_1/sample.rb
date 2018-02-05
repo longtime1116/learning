d40b56004b4356c0176518e5616723259a071f86
shumo.tokinaga <longtime1116@gmail.com>
2018-02-05 19:35:13 +0900

    extract amount_for and move to Rental class

--- refactoring_ruby/chapter_1/sample.rb
+++ refactoring_ruby/chapter_1/sample.rb
@@ -17,6 +17,21 @@ class Rental
   def initialize(movie, days_rented)
     @movie, @days_rented = movie, days_rented
   end
+
+  def charge
+      result = 0
+      case movie.price_code
+      when Movie::REGULAR
+        result += 2
+        result += (days_rented - 2) * 1.5 if days_rented > 2
+      when Movie::NEW_RELEASE
+        result += days_rented * 3
+      when Movie::CHILDRENS
+        result += 1.5
+        result += (days_rented - 3) * 1.5 if days_rented > 3
+      end
+      result
+  end
 end
 
 class Customer
@@ -35,7 +50,7 @@ class Customer
     total_amount, frequent_renter_points = 0, 0
     result = "Rental Record for #{@name}\n"
     @rentals.each do |element|
-      this_amount = amount_for(element)
+      this_amount = element.charge
 
       # add frequent renter points
       frequent_renter_points += 1
@@ -52,19 +67,5 @@ class Customer
     result += "Amount owed is #{total_amount}\n"
     result += "You earned #{frequent_renter_points} frequent renter points"
     result
-  end
-
-  def amount_for(rental)
-      result = 0
-      case rental.movie.price_code
-      when Movie::REGULAR
-        result += 2
-        result += (rental.days_rented - 2) * 1.5 if rental.days_rented > 2
-      when Movie::NEW_RELEASE
-        result += rental.days_rented * 3
-      when Movie::CHILDRENS
-        result += 1.5
-        result += (rental.days_rented - 3) * 1.5 if rental.days_rented > 3
-      end
   end
 end
