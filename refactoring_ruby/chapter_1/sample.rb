73bc76e95a304caaa7aafe3f9864af93a30a7f37
shumo.tokinaga <longtime1116@gmail.com>
2018-02-06 19:58:52 +0900

    add html_statement

--- refactoring_ruby/chapter_1/sample.rb
+++ refactoring_ruby/chapter_1/sample.rb
@@ -68,6 +68,20 @@ class Customer
     result
   end
 
+  def html_statement
+    result = "<h1>Rentals for <em>#{@name}</em></h1><p>\n"
+    @rentals.each do |element|
+      # show figures for this rental
+      result += "\t" + each.movie.title + ": " + element.charge.to_s + "<br>\n"
+    end
+    # add footer lines
+    result += "<p>You owe <em>#{total_charge}</em><p>\n"
+    result += "On this rental you earned " +
+      "<em>#{total_frequent_renter_points}</em> " +
+      "frequent renter points<p>"
+    result
+  end
+
   private
 
   def total_charge
