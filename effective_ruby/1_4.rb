6748e6a7dd71971f87df41f7ebc1e6bfd167a05b
longtime1116 <longtime1116@gmail.com>
2018-03-11 13:14:34 +0900

    update 1_4

--- effective_ruby/1_4.rb
+++ effective_ruby/1_4.rb
@@ -11,6 +11,11 @@ p add("com")
 p Defaults::HOGE  # [aaa.com, bbb.com] となってしまっている！！！
 
 
+
+
+
+
+
 # freeze しないで試してみる
 CONST = ["a", "b"]
 
@@ -22,3 +27,28 @@ p CONST[0].object_id #=> object_id が変わる！
 
 
 # ということで、object_id が変わるような変更は freeze するとできないが、object_id が変わらないならば変更可能。ポインタが変わらないのならOKみたいな感じか。
+
+
+
+
+
+CONST2 = ["a", "b"].map!(&:freeze).freeze
+p CONST2[0].object_id
+#CONST2[0] << "c"     #=> エラーになる!
+#p CONST2[0].object_id
+#CONST2[0] = "c"     #=> エラーになる!
+#p CONST2[0].object_id
+
+# ということで、中身も freeze すれば書き換えは不可能にできる
+
+
+
+
+
+CONST2 = ["c"]
+p CONST2   #=> warning は出るが書き換えることができてしまう...
+module Piyo
+  CONST2 = ["c"].map!(&:freeze).freeze
+end
+Piyo.freeze
+#Piyo::CONST2 = ["d"]   # これでようやくエラーになる
