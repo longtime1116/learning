396a00a0f38272be44f4fe58f44c38737e1980d5
shumo.tokinaga <longtime1116@gmail.com>
2018-03-14 17:05:41 +0900

    typecode to polymorphic(after)

---
+++ refactoring_ruby/chapter_8/8_12_2.rb
@@ -0,0 +1,48 @@
+# after ポリモーフィズム
+
+module OfficeWorker
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def salary
+    500
+  end
+end
+
+class Manager
+  include OfficeWorker
+
+  def salary
+    3000
+  end
+end
+
+class Engineer
+  include OfficeWorker
+
+  def salary
+    2000
+  end
+end
+
+class Sales
+  include OfficeWorker
+
+  def salary
+    1500
+  end
+end
+
+
+
+
+tom = Manager.new("Tom")
+p tom.name
+p tom.salary
+
+kevin = Engineer.new("Kevin")
+p kevin.name
+p kevin.salary
