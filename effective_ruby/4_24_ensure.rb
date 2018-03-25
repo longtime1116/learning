09e3af731e821cd2ecec690aa086e52e8992d19e
longtime1116 <longtime1116@gmail.com>
2018-03-25 11:24:10 +0900

    4_24 use ensure like File.open

---
+++ effective_ruby/4_24_ensure.rb
@@ -0,0 +1,40 @@
+class Lock
+  attr_accessor :lock_state
+
+  def initialize
+    @lock_state = false
+  end
+
+  def self.acquire
+    lock = new
+    p "# after create"
+    p lock.lock_state
+    lock.exclusive_lock!
+    p "# after exclusive_lock!"
+    p lock.lock_state
+    yield(lock)
+  ensure
+    lock.unlock! if lock
+  end
+
+  def exclusive_lock!
+    @lock_state = true
+  end
+
+  def unlock!
+  p "# just before unlock!"
+    p lock_state
+    @lock_state = false
+  p "# just after unlock!"
+    p lock_state
+  end
+end
+
+Lock.acquire do |lock|
+  p "# in block(before raise)"
+  p lock.lock_state
+  p "# raise!!!!!"
+  raise
+end
+
+# File.open do {|file| } も同じように ensure で close している
