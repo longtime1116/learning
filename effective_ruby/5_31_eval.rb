0d2c8ac010b8455dd9ec076809f4d297e03c9dcf
longtime1116 <longtime1116@gmail.com>
2018-04-12 08:41:27 +0900

    instance_eval

--- effective_ruby/5_31_eval.rb
+++ effective_ruby/5_31_eval.rb
@@ -11,3 +11,25 @@ def hoge_scope
 end
 
 eval("p x", hoge_scope)
+
+
+
+####################################################################################
+class Widget
+  def initialize(name)
+    @name = name
+  end
+end
+
+w = Widget.new("Gear")
+
+# その instance をレシーバとしてブロックを実行する
+# instance_eval のなかで method 定義すると、特異メソッドを作ることになる
+p w.instance_eval {@name}
+w.instance_eval do
+  def in_stock?
+    false
+  end
+end
+
+p w.singleton_methods(false)
