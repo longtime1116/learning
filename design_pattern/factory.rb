9a0a3d64d2f5eacb820a43bf68e2133aa5b5ad57
longtime1116 <longtime1116@gmail.com>
2018-04-26 08:43:11 +0900

    add Frog

--- design_pattern/factory.rb
+++ design_pattern/factory.rb
@@ -4,21 +4,33 @@ class Duck
   end
 
   def speak
-    puts ("#{@name} はガーガー鳴いています。")
+    puts ("#{@name} はガーガー")
   end
 end
 
+class Frog
+  def initialize(name)
+    @name = name
+  end
+
+  def speak
+    puts ("#{@name} はゲロゲロ")
+  end
+
+end
+
 class Pond
-  def initialize(number_ducks)
-    @ducks = []
-    number_ducks.times do |i|
-      @ducks << Duck.new("Duck#{i}") 
+  def initialize(number_animals, animal_class)
+    @animals = []
+    number_animals.times do |i|
+      @animals << animal_class.new("#{animal_class.to_s}#{i}") 
     end
   end
 
   def simulate
-    @ducks.each {|duck| duck.speak}
+    @animals.each {|animal| animal.speak}
   end
 end
 
-Pond.new(3).simulate
+Pond.new(3, Duck).simulate
+Pond.new(5, Frog).simulate
