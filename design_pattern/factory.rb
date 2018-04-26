2103f6cc5f403cdbbbb4f96313b5db33e4026c19
longtime1116 <longtime1116@gmail.com>
2018-04-26 09:17:02 +0900

    add tiger and tree

--- design_pattern/factory.rb
+++ design_pattern/factory.rb
@@ -1,3 +1,4 @@
+# 池に住むものたち
 class Duck
   def initialize(name)
     @name = name
@@ -38,15 +39,72 @@ class WaterLily
   end
 end
 
-class Pond
-  def initialize(number_animals, animal_class, number_plants, plant_class)
+# 密林に住むものたち
+class Tiger
+  def initialize(name)
+    @name = name
+  end
+
+  def speak
+    puts "#{@name} はガオー"
+  end
+end
+
+class Tree
+  def initialize(name)
+    @name = name
+  end
+
+  def grow
+    puts "#{@name} は密集"
+  end
+end
+
+class PondOrganismFactory
+  def new_animal(name)
+    Duck.new(name)
+  end
+
+  def animal_type
+    Duck.to_s
+  end
+
+  def new_plant(name)
+    Algae.new(name)
+  end
+
+  def plant_type
+    Algae.to_s
+  end
+end
+
+class JungleOrganismFactory
+  def new_animal(name)
+    Tiger.new(name)
+  end
+
+  def animal_type
+    Tiger.to_s
+  end
+
+  def new_plant(name)
+    Tree.new(name)
+  end
+
+  def plant_type
+    Tree.to_s
+  end
+end
+
+class Habitat
+  def initialize(number_animals, number_plants, organism_factory)
     @animals = []
     number_animals.times do |i|
-      @animals << animal_class.new("#{animal_class.to_s}#{i}") 
+      @animals << organism_factory.new_animal("#{organism_factory.animal_type}#{i}")
     end
     @plants = []
     number_plants.times do |i|
-      @plants << plant_class.new("#{plant_class.to_s}#{i}") 
+      @plants << organism_factory.new_plant("#{organism_factory.plant_type}#{i}")
     end
   end
 
@@ -56,5 +114,10 @@ class Pond
   end
 end
 
-Pond.new(3, Duck, 2, Algae).simulate
-Pond.new(5, Frog, 4, WaterLily).simulate
+#Pond.new(3, Duck, 2, Algae).simulate
+#Pond.new(5, Frog, 4, WaterLily).simulate
+
+Habitat.new(3, 2, PondOrganismFactory.new).simulate
+Habitat.new(1, 5, JungleOrganismFactory.new).simulate
+
+
