cd177666466ed340386a9ee7da3b812c4df1d017
longtime1116 <longtime1116@gmail.com>
2018-05-03 12:54:43 +0900

    scope

---
+++ meta/scope.rb
@@ -0,0 +1,13 @@
+val = "hoge"
+p "top レベルで定義した変数 val: #{val}"
+Hoge = Class.new do
+  p "クラス定義の中から val: #{val} を参照できる"
+  define_method :call_val do
+    p "クラス定義の中のメソッドの中から val: #{val} を参照できる"
+  end
+end
+
+Hoge.new.call_val
+
+# class や def を使うと、scope を共有できない。
+# このように共有する方法をフラットスコープと呼ぶ
