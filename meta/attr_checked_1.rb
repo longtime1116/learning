b84e02b5b73b2ef2962c86c302ed03f560d394e9
longtime1116 <longtime1116@gmail.com>
2018-05-07 02:22:00 +0900

    add attr_checked method by using extend

---
+++ meta/attr_checked_1.rb
@@ -0,0 +1,27 @@
+module CheckedAttributes
+  def attr_checked(attribute, &validation)
+    define_method "#{attribute}=" do |value|
+      raise "Invalid attribute" unless validation.call(value)
+      instance_variable_set("@#{attribute}", value)
+    end
+
+    define_method "#{attribute}" do
+      instance_variable_get("@#{attribute}")
+    end
+  end
+end
+
+class Person
+  extend CheckedAttributes
+  attr_checked :age do |v|
+    v >= 18
+  end
+end
+
+p Person.ancestors
+p Person.singleton_class.ancestors
+hoge = Person.new
+hoge.age = 18
+p hoge.age
+hoge.age = 17
+p hoge.age
