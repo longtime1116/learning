3db0dc03e4d13badc6d50265a8f715d320e86794
shumo.tokinaga <longtime1116@gmail.com>
2019-02-27 18:43:04 +0900

    add lib and .gitignore

---
+++ deep-learning-from-scratch/lib/functions.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def step_function(x):
+    y = x > 0
+    return y.astype(np.int)
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def relu(x):
+    return np.maximum(0, x)
