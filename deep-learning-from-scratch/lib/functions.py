fa2252741f9292974d082e0631861e1fb50d4a4c
shumo.tokinaga <longtime1116@gmail.com>
2019-03-13 17:15:24 +0900

    numerical diff

--- deep-learning-from-scratch/lib/functions.py
+++ deep-learning-from-scratch/lib/functions.py
@@ -45,5 +45,8 @@ def cross_entropy_error(y, t):
     return -np.sum(t * np.log(y + 1e-7)) / batch_size
 
 
+def numerical_diff(f, x):
+    h = 1e-4 # 0.0001
+    return (f(x+h) - f(x-h)) / (2*h)
 
 
