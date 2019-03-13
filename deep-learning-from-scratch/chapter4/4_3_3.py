5947d175d9d973d5150d279f330a78927bf67854
shumo.tokinaga <longtime1116@gmail.com>
2019-03-13 17:27:39 +0900

    numerical gradient

---
+++ deep-learning-from-scratch/chapter4/4_3_3.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pylab as plt
+import sys,os
+sys.path.append("../lib")
+sys.path.append(os.pardir)
+import functions
+from dataset.mnist import load_mnist
+
+def function_2(x):
+    return x[0]**2 + x[1]**2
+
+def numerical_gradient(f, x):
+    h = 1e-4 # 0.0001
+    grad = np.zeros_like(x)
+
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1 - fxh2) / (2 * h)
+        x[idx] = tmp_val
+
+    return grad
+
+print(numerical_gradient(function_2, np.array([3.0, 4.0])))
+print(numerical_gradient(function_2, np.array([0.0, 2.0])))
+print(numerical_gradient(function_2, np.array([-1.0, -2.0])))
