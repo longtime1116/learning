a1afd792e4f4570c1ce90bc698b0564d39ca107f
shumo.tokinaga <longtime1116@gmail.com>
2019-03-20 18:35:02 +0900

    numerical gradient

--- deep-learning-from-scratch/lib/functions.py
+++ deep-learning-from-scratch/lib/functions.py
@@ -49,26 +49,6 @@ def numerical_diff(f, x):
     h = 1e-4 # 0.0001
     return (f(x+h) - f(x-h)) / (2*h)
 
-def numerical_gradient(f, x):
-    h = 1e-4 # 0.0001
-    grad = np.zeros_like(x)
-
-    print(x)
-    for idx in range(x.size):
-        tmp_val = x[idx]
-        x[idx] = tmp_val + h
-        fxh1 = f(x)
-
-        x[idx] = tmp_val + h
-        fxh1 = f(x)
-
-        x[idx] = tmp_val - h
-        fxh2 = f(x)
-
-        grad[idx] = (fxh1 - fxh2) / (2 * h)
-        x[idx] = tmp_val
-
-    return grad
 
 def numerical_gradient_matrix(f, x):
     h = 1e-4 # 0.0001
@@ -77,7 +57,9 @@ def numerical_gradient_matrix(f, x):
     row, column = x.shape
     # nditer を使うと一重ループでいける
     for i in range(row):
+        #print("i: " + str(i))
         for j in range(column):
+            #print(" j: " + str(j))
             tmp_val = x[i][j]
             x[i][j] = tmp_val + h
             fxh1 = f(x)
@@ -90,4 +72,45 @@ def numerical_gradient_matrix(f, x):
 
             grad[i][j] = (fxh1 - fxh2) / (2 * h)
             x[i][j] = tmp_val
+    return grad
+
+#def numerical_gradient(f, x):
+#    h = 1e-4 # 0.0001
+#    grad = np.zeros_like(x)
+#
+#    print(x)
+#    for idx in range(x.size):
+#        tmp_val = x[idx]
+#        x[idx] = tmp_val + h
+#        fxh1 = f(x)
+#
+#        x[idx] = tmp_val + h
+#        fxh1 = f(x)
+#
+#        x[idx] = tmp_val - h
+#        fxh2 = f(x)
+#
+#        grad[idx] = (fxh1 - fxh2) / (2 * h)
+#        x[idx] = tmp_val
+#
+#    return grad
+
+def numerical_gradient(f, x):
+    h = 1e-4 # 0.0001
+    grad = np.zeros_like(x)
+
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    while not it.finished:
+        idx = it.multi_index
+        tmp_val = x[idx]
+        x[idx] = float(tmp_val) + h
+        fxh1 = f(x) # f(x+h)
+
+        x[idx] = tmp_val - h 
+        fxh2 = f(x) # f(x-h)
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+
+        x[idx] = tmp_val # 値を元に戻す
+        it.iternext()   
+
     return grad
