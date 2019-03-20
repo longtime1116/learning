dd2b3c5e183caa5950224ce9b8b5c4afbcc5ffb8
shumo.tokinaga <longtime1116@gmail.com>
2019-03-20 17:52:19 +0900

    update 4_3_3

--- deep-learning-from-scratch/chapter4/4_3_3.py
+++ deep-learning-from-scratch/chapter4/4_3_3.py
@@ -29,6 +29,30 @@ def numerical_gradient(f, x):
 
     return grad
 
-print(numerical_gradient(function_2, np.array([3.0, 4.0])))
-print(numerical_gradient(function_2, np.array([0.0, 2.0])))
-print(numerical_gradient(function_2, np.array([-1.0, -2.0])))
+#print(numerical_gradient(function_2, np.array([3.0, 4.0])))
+#print(numerical_gradient(function_2, np.array([0.0, 2.0])))
+#print(numerical_gradient(function_2, np.array([-1.0, -2.0])))
+
+
+def gradient_descent(f, init_x, lr=0.01, step_num=100):
+    x = init_x
+    for i in range(step_num):
+        grad = numerical_gradient(f, x)
+        #print(x)
+        x -= lr * grad
+    return x
+
+
+init_x = np.array([-3.0, 4.0])
+x = gradient_descent(function_2, init_x=init_x, lr=0.1, step_num=100)
+#print(x)
+
+init_x = np.array([-3.0, 4.0])
+x = gradient_descent(function_2, init_x=init_x, lr=10, step_num=100)
+#print(x)
+
+init_x = np.array([-3.0, 4.0])
+x = gradient_descent(function_2, init_x=init_x, lr=1e-10, step_num=100)
+#print(x)
+
+
