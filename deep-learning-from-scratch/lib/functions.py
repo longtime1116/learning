bf0b02f2a2072c0b03e262215d35d0c37ec60aa2
shumo.tokinaga <longtime1116@gmail.com>
2019-03-20 17:52:48 +0900

    add numerical gradient

--- deep-learning-from-scratch/lib/functions.py
+++ deep-learning-from-scratch/lib/functions.py
@@ -49,4 +49,45 @@ def numerical_diff(f, x):
     h = 1e-4 # 0.0001
     return (f(x+h) - f(x-h)) / (2*h)
 
+def numerical_gradient(f, x):
+    h = 1e-4 # 0.0001
+    grad = np.zeros_like(x)
 
+    print(x)
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1 - fxh2) / (2 * h)
+        x[idx] = tmp_val
+
+    return grad
+
+def numerical_gradient_matrix(f, x):
+    h = 1e-4 # 0.0001
+    grad = np.zeros_like(x)
+
+    row, column = x.shape
+    # nditer を使うと一重ループでいける
+    for i in range(row):
+        for j in range(column):
+            tmp_val = x[i][j]
+            x[i][j] = tmp_val + h
+            fxh1 = f(x)
+
+            x[i][j] = tmp_val + h
+            fxh1 = f(x)
+
+            x[i][j] = tmp_val - h
+            fxh2 = f(x)
+
+            grad[i][j] = (fxh1 - fxh2) / (2 * h)
+            x[i][j] = tmp_val
+    return grad
