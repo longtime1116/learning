b209dc6a407fb96404da97f583423475dae1c74a
shumo.tokinaga <longtime1116@gmail.com>
2019-05-09 17:42:19 +0900

    chapter5

--- deep-learning-from-scratch/lib/functions.py
+++ deep-learning-from-scratch/lib/functions.py
@@ -20,12 +20,22 @@ def softmax_simple(x):
     return y
 
 # 順番が変わらないまま、和が1という性質を利用して確率としてみなせる
+#def softmax(x):
+#    C = np.max(x)
+#    exp_x = np.exp(x - C)
+#    sum_exp_x = np.sum(exp_x)
+#    y = exp_x / sum_exp_x
+#    return y
+
 def softmax(x):
-    C = np.max(x)
-    exp_x = np.exp(x - C)
-    sum_exp_x = np.sum(exp_x)
-    y = exp_x / sum_exp_x
-    return y
+    if x.ndim == 2:
+        x = x.T
+        x = x - np.max(x, axis=0)
+        y = np.exp(x) / np.sum(np.exp(x), axis=0)
+        return y.T
+
+    x = x - np.max(x) # オーバーフロー対策
+    return np.exp(x) / np.sum(np.exp(x))
 
 def mean_squared_error(y, t):
     return 0.5 * np.sum((y-t) ** 2)
@@ -114,3 +124,57 @@ def numerical_gradient(f, x):
         it.iternext()   
 
     return grad
+
+
+class Affine:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        self.x = x
+        out = np.dot(x, self.W) + self.b
+        return out
+
+    def backward(self, dout):
+        dx = np.dot(dout, self.W.T)
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+        return dx
+
+class Relu:
+    def __init__(self):
+        self.mask = None
+
+    def forward(self, x):
+        self.mask = (x <= 0)
+        out = x.copy()
+        out[self.mask]= 0
+        return out
+
+    def backward(self, dout):
+        dout[self.mask] = 0
+        dx = dout
+        return dx
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None #損失
+        self.y = None #softmaxの出力
+        self.t = None # 教師データ
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
+        return dx
+
+
