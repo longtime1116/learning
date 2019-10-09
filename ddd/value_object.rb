4636d7277988ff2f148862b8e1bdafc23f8599bd
shumo.tokinaga <longtime1116@gmail.com>
2019-10-09 16:56:55 +0900

    value_object.rb

---
+++ ddd/value_object.rb
@@ -0,0 +1,38 @@
+class Money
+  attr_accessor :third
+  attr_accessor :second
+  attr_accessor :first
+  attr_accessor :unit
+  def initialize(third, second, first, unit)
+    @third = third
+    @second = second
+    @first = first
+    @unit= unit
+  end
+  def to_s
+    (@third * 100 + @second * 10 + @first * 1).to_s + @unit
+  end
+end
+
+
+class User
+  def initialize(money)
+    save_money(money)
+  end
+
+  def money
+    @amount.to_s + @unit
+  end
+  private
+  def save_money(money)
+    @amount = money.third * 100 + money.second * 10 + money.first * 1
+    @unit = money.unit
+  end
+
+
+end
+
+m = Money.new(2,1,3,"yen")
+p m.to_s
+user = User.new(m)
+p user.money
